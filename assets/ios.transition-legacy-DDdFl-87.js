fe4eeac35c220aa732203c1022662aa97f34270c
Jemjem7 <benzjeramtinga@gmail.com>
2025-02-18 08:49:57 +0800

    Updates

--- assets/ios.transition-legacy-DDdFl-87.js
+++ assets/ios.transition-legacy-DDdFl-87.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CcpNGZke.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-wSYK83p-.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/keyboard2-legacy-DtnwM28t.js
+++ assets/keyboard2-legacy-DtnwM28t.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CcpNGZke.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-wSYK83p-.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
