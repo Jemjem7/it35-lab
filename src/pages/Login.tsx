53e278bc585257e73f2e2706e4489885775184e4
Jemjem7 <benzjeramtinga777gmail.com>
2025-03-06 10:36:53 +0800

    added comment and fixed eye(show and hide password)

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -10,11 +10,20 @@ import {
   IonToolbar,
   IonText,
   useIonRouter,
-  IonAvatar
+  IonAvatar,
+  IonIcon  // Added IonIcon for the eye toggle
 } from '@ionic/react';
+import { eye, eyeOff } from 'ionicons/icons'; // Importing icons for show/hide password
+import { useState } from 'react'; // Importing useState for managing password visibility
 
 const Login: React.FC = () => {
   const navigation = useIonRouter();
+  const [showPassword, setShowPassword] = useState(false); // State to track password visibility
+
+  // Function to toggle password visibility
+  const togglePasswordVisibility = () => {
+    setShowPassword((prevState) => !prevState);
+  };
 
   const doLogin = () => {
     navigation.push('/it35-lab/app', 'forward', 'replace');
@@ -30,11 +39,9 @@ const Login: React.FC = () => {
       <IonContent className="ion-padding ion-text-center ion-justify-content-center">
         <div className="login-form">
 
-
         <IonAvatar className="ion-margin-bottom">
          <img alt="Silhouette of a person's head" src="https://ionicframework.com/docs/img/demos/avatar.svg" />
         </IonAvatar>
-
 
           <IonItem>
             <IonLabel position="floating">Username:</IonLabel>
@@ -43,12 +50,16 @@ const Login: React.FC = () => {
           
           <IonItem>
             <IonLabel position="floating">Password:</IonLabel>
-            <IonInput type="password" />
+            <IonInput type={showPassword ? "text" : "password"} /> {/* Toggles password visibility */}
+            <IonButton fill="clear" slot="end" onClick={togglePasswordVisibility}>
+            <IonIcon icon={showPassword ? eyeOff : eye} /> {/* Changes icon based on visibility state */}
+            </IonButton>
           </IonItem>
-          
+
           <IonButton onClick={doLogin} expand="full">
             Login
           </IonButton>
+
         </div>
       </IonContent>
     </IonPage>
@@ -56,3 +67,7 @@ const Login: React.FC = () => {
 };
 
 export default Login;
+function setPasswordVisible(arg0: (prevState: any) => boolean) {
+  throw new Error('Function not implemented.');
+}
+
