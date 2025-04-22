3b2756a9d09e076a8fa6a6cfbe3bd45226516b60
Jemjem <gitgakko@gmail.com>
2025-04-22 09:08:08 +0800

    Updates

--- assets/ios.transition-legacy-mduPLm7D.js
+++ assets/ios.transition-legacy-mduPLm7D.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DURU5cyQ.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-CTZxUL7W.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
