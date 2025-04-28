85d8dd19bb6ced041ab7743edb0270bc66ef3ee7
Jemjem7 <gitgakko@gmail.com>
2025-04-28 21:46:11 +0800

    Updates

--- assets/md.transition-legacy-D1WOy6VC.js
+++ assets/md.transition-legacy-D1WOy6VC.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CCU_uSvu.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-DpGuWSPs.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
