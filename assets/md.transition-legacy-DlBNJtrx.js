21249d625861dacd8db51b4f9424eeb8e15c79fd
jemjem7 <gitgakko@gmail.com>
2025-04-29 18:50:10 +0800

    Updates

--- assets/md.transition-legacy-DlBNJtrx.js
+++ assets/md.transition-legacy-DlBNJtrx.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Bo93klLE.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-bMIBLLxY.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
