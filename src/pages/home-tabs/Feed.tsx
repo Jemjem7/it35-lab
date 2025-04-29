32a6f3caa5fd86c02c2e5634f4005ffef61dfef2
jemjem7 <gitgakko@gmail.com>
2025-04-29 18:38:49 +0800

    added

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -1,64 +1,107 @@
-import { 
+import React from 'react';
+import {
   IonButtons,
-    IonContent, //
-    IonHeader, //
-    IonMenuButton, 
-    IonPage, //
-    IonTitle, //
-    IonToolbar, //
-    IonList,
-    IonItem,
-    IonLabel,
-    IonAvatar,
-
+  IonContent,
+  IonFab,
+  IonFabButton,
+  IonFabList,
+  IonHeader,
+  IonIcon,
+  IonMenuButton,
+  IonPage,
+  IonSearchbar,
+  IonTitle,
+  IonToolbar,
+  IonCard,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonCardContent
 } from '@ionic/react';
+import { add, colorPalette, document, globe } from 'ionicons/icons';
 
 const Feed: React.FC = () => {
-  const feedData = [
+  const handleSearch = (e: any) => {
+    console.log('Searching for: ', e.target.value);
+  };
 
-    { id: 1, user: "Unknown", content: "Just uploaded a new manga artwork!", time: "8 hours ago" },
-    { id: 2, user: "Unknown", content: "Added new project!", time: "1 hour ago" },
-    { id: 3, user: "Unknown", content: "edited a project!", time: "8 mins ago" },
-    { id: 4, user: "Unknown", content: "visit my repo!", time: "1 day ago" },
-    { id: 5, user: "Unknown", content: "check my new security project!", time: "3 days ago " },
-
-
-   ]
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonButtons slot='start'>
-            <IonMenuButton></IonMenuButton>
+            <IonMenuButton />
           </IonButtons>
           <IonTitle>Feed</IonTitle>
         </IonToolbar>
+        <IonSearchbar
+          placeholder="Search Feed..."
+          onIonInput={handleSearch}
+          debounce={500} // optional debounce
+        />
       </IonHeader>
-      <IonContent fullscreen>
-        <IonList>
-          {feedData.map((item) => (
-            <IonItem key={item.id}>
-              <IonAvatar slot="start">
-                <img src="https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9da77e3b?d=identicon&f=y" />
-              </IonAvatar>
-              <IonLabel>
-                <h2>{item.user}</h2>
-                <p>{item.content}</p>
-                <p>{item.time}</p>
-              </IonLabel>
-            </IonItem>
-          ))}
-        </IonList>
-        <div
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '100%',
-          }}
-        >
-          Feed
-        </div>
+
+      <IonContent fullscreen className="ion-padding">
+
+        <IonCard>
+          <img
+            alt="Silhouette of mountains"
+            src="https://ionicframework.com/docs/img/demos/card-media.png"
+          />
+          <IonCardHeader>
+            <IonCardTitle>ONE PIECE: FILM RED</IonCardTitle>
+          </IonCardHeader>
+
+          <IonCardContent>
+          Uta, the most beloved singer on the planet, is known for hiding her own identity when performing. Now, for the first time, she will reveal herself to the world. All of Uta's fans, including the Straw Hats led by Luffy, await.
+          </IonCardContent>
+        </IonCard>
+
+        <IonCard>
+          <img
+            alt="Silhouette of mountains"
+            src="https://ionicframework.com/docs/img/demos/card-media.png"
+          />
+          <IonCardHeader>
+            <IonCardTitle>ONE PIECE: DRESSROSA ARC</IonCardTitle>
+          </IonCardHeader>
+
+          <IonCardContent>
+          The Straw Hats and Law reach Dressrosa where they plan to bring down Doflamingo. A sequence of unexpected events shakes the world order, and Doflamingo proves to be a challenge that even some of the strongest warriors cannot overcome.
+          </IonCardContent>
+        </IonCard>
+
+        <IonCard>
+          <img
+            alt="Silhouette of mountains"
+            src="https://ionicframework.com/docs/img/demos/card-media.png"
+          />
+          <IonCardHeader>
+            <IonCardTitle>ONE PIECE: EGGHEAD ARC</IonCardTitle>
+          </IonCardHeader>
+
+          <IonCardContent>
+          Luffy arrived on the island of the future, Egghead, where he met Vegapunk. Given that Egghead followed the future theme, fans knew that this would be an arc focused on technology, as well as many inventions made by Vegapunk over the years.
+          </IonCardContent>
+        </IonCard>
+
+        {/* Floating Action Button */}
+        <IonFab vertical="bottom" horizontal="end" slot="fixed">
+          <IonFabButton size="small">
+            <IonIcon icon={add} />
+          </IonFabButton>
+          <IonFabList side="top">
+            <IonFabButton>
+              <IonIcon icon={document} />
+            </IonFabButton>
+            <IonFabButton>
+              <IonIcon icon={colorPalette} />
+            </IonFabButton>
+            <IonFabButton>
+              <IonIcon icon={globe} />
+            </IonFabButton>
+          </IonFabList>
+        </IonFab>
       </IonContent>
     </IonPage>
   );
