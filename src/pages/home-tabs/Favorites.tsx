7748098a286b3f4f14d419e7482e5524bb6d2ce9
Benz Jeram Tinga <143716507+Jemjem7@users.noreply.github.com>
2025-04-29 21:56:41 +0800

    Revert "Feature/feed"

--- src/pages/home-tabs/Favorites.tsx
+++ src/pages/home-tabs/Favorites.tsx
@@ -1,90 +1,34 @@
-import {
+import { 
   IonButtons,
-  IonContent,
-  IonHeader,
-  IonMenuButton,
-  IonPage,
-  IonTitle,
-  IonToolbar
+    IonContent, 
+    IonHeader, 
+    IonMenuButton, 
+    IonPage, 
+    IonTitle, 
+    IonToolbar 
 } from '@ionic/react';
 
 const Favorites: React.FC = () => {
-  const artImages = [
-    {
-      src: 'https://i.postimg.cc/Znd1M0V1/vagabond.jpg',
-      title: 'Vagabond',
-      link: null
-    },
-    {
-      src: 'https://i.postimg.cc/jdKLj7TX/kimjunggi.jpg',
-      title: 'Kim Jung Gi',
-      link: null
-    },
-    {
-      src: 'https://i.postimg.cc/mr9XNY2R/gawxart.jpg',
-      title: 'Gawx Art',
-      link: 'https://www.instagram.com/gawx_art/?hl=en'
-    }
-  ];
-
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonButtons slot="start">
-            <IonMenuButton />
+          <IonButtons slot='start'>
+            <IonMenuButton></IonMenuButton>
           </IonButtons>
           <IonTitle>Favorites</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <div style={{ padding: '16px' }}>
-          <h2 style={{ textAlign: 'center' }}>My Favorite Manga/Artists</h2>
-          <div
-            style={{
-              display: 'grid',
-              gridTemplateColumns: 'repeat(auto-fill, minmax(150px, 1fr))',
-              gap: '16px',
-              marginTop: '20px'
-            }}
-          >
-            {artImages.map((item, index) => (
-              <div key={index} style={{ textAlign: 'center' }}>
-                {item.link ? (
-                  <a href={item.link} target="_blank" rel="noopener noreferrer">
-                    <img
-                      src={item.src}
-                      alt={item.title}
-                      style={{
-                        width: '100%',
-                        borderRadius: '8px',
-                        boxShadow: '0 2px 8px rgba(0,0,0,0.15)'
-                      }}
-                    />
-                  </a>
-                ) : (
-                  <img
-                    src={item.src}
-                    alt={item.title}
-                    style={{
-                      width: '100%',
-                      borderRadius: '8px',
-                      boxShadow: '0 2px 8px rgba(0,0,0,0.15)'
-                    }}
-                  />
-                )}
-                <p
-                  style={{
-                    marginTop: '8px',
-                    fontWeight: 'bold',
-                    color: '#fff'
-                  }}
-                >
-                  {item.title}
-                </p>
-              </div>
-            ))}
-          </div>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100%',
+          }}
+        >
+          Favorites
         </div>
       </IonContent>
     </IonPage>
--- src/pages/home-tabs/Search.tsx
+++ src/pages/home-tabs/Search.tsx
@@ -1,49 +1,39 @@
 import { 
-  IonButtons,
-    IonContent, 
-    IonHeader, 
-    IonMenuButton, 
-    IonPage, 
-    IonTitle, 
-    IonToolbar, 
-    IonSearchbar,
-    IonGrid,
-    IonRow,
-    IonCol,
-
-} from '@ionic/react';
-
-const Search: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonButtons slot='start'>
-            <IonMenuButton></IonMenuButton>
-          </IonButtons>
-          <IonTitle>Search</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-         
-      <IonGrid>
-      <IonRow className="ion-justify-content-center">
-
-            <IonCol size="12" sizeMd="8" sizeLg="6" className="ion-padding">
-            <IonSearchbar 
-              placeholder="Search"
-              debounce={0} // Optional: Optional debounce for input delay
-              showClearButton="focus"
-              
-       
-               />
-             </IonCol>
-          </IonRow>
-        </IonGrid>
-       
-      </IonContent>
-    </IonPage>
-  );
-};
-
-export default Search;
+    IonButtons,
+      IonContent, 
+      IonHeader, 
+      IonMenuButton, 
+      IonPage, 
+      IonTitle, 
+      IonToolbar 
+  } from '@ionic/react';
+  
+  const Search: React.FC = () => {
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot='start'>
+              <IonMenuButton></IonMenuButton>
+            </IonButtons>
+            <IonTitle>Search</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent fullscreen>
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              height: '100%',
+            }}
+          >
+            Search
+          </div>
+  
+        </IonContent>
+      </IonPage>
+    );
+  };
+  
+  export default Search;
